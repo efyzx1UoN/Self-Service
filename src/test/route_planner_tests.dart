39355345550e328acf94ba61b168faaefef9a114
psytm1 <psytm1@nottingham.ac.uk>
2021-02-12 09:38:29 +0000

    Created new file route_planner_tests

--- src/test/route_planner_tests.dart
+++ src/test/route_planner_tests.dart
@@ -1,8 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_app/main.dart';
 
 void main() {
-  // test('title',() {
-  //
-  //
-  //
-  // });
+  group("Input Validation", () {
+    testWidgets('New Route', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(MyApp());
+
+      // Tap the New Route Plan button.
+      await tester.tap(find.widgetWithText(TextButton, "New Route Plan"));
+      await tester.pumpAndSettle();
+
+      // Tap the New Route Plan button.
+      await tester.tap(find.byKey("StartingLocationInput")));
+      await tester.pumpAndSettle();
+      
+    });
+  });
 }
