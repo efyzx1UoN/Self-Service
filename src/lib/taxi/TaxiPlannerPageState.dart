92f8c1d68470f83869b2c3b3a77a3c3c99ba7dc9
efyzx1UoN <efyzx1@nottingham.ac.uk>
2021-03-15 03:44:55 +0800

    Full Screen

--- src/lib/taxi/TaxiPlannerPageState.dart
+++ src/lib/taxi/TaxiPlannerPageState.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/taxi/routeDirections_taxi.dart';
 import 'package:flutter_app/taxi/taxiAlbum.dart';
@@ -210,8 +213,9 @@ class RoutePlannerFormState_taxi extends ObserverState {
               child: _m_geoTracker.m_locationCoordinates == null
                   ? Container()
                   : _m_geoTracker.m_map,
-              height: geoTracker.CONTAINER_ONE_DIMENSION,
-              width: geoTracker.CONTAINER_ONE_DIMENSION,
+              height: MediaQuery.of(context).size.height,
+              width:MediaQuery.of(context).size.width
+              //geoTracker.CONTAINER_ONE_DIMENSION,
             ),
             Visibility(
               maintainInteractivity: false,
